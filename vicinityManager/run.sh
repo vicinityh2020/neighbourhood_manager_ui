47948ee340e24236fec836a208fc1856d9cfea7c
Jorge Almela <jorge.almela@bavenir.eu>
2019-03-08 13:51:42 +0100

    Add right ports to docker; fix running script

--- vicinityManager/run.sh
+++ vicinityManager/run.sh
@@ -1,21 +1,20 @@
-#!/bin/sh
-usage="$(basename "$0") [-h] [-p port -e env -d dns_name]
- -- Builds vcnt-ui docker
+#!/bin/bash
+usage="$(basename "$0") [-h] [-e env -d dns_name]
+-- Builds vcnt-ui docker
  -- Examples
- -- Production: ./run.sh -p 80 -e prod -d www.example.com
- -- Development: ./run.sh -p 8888 -e dev -d www.example.com
- -- Local: ./run.sh [ -p XXXX ]
+ -- Production: ./run.sh -e prod -d www.example.com
+ -- Development: ./run.sh -e dev -d www.example.com
+ -- Local: ./run.sh [ without arguments, access on localhost:8080 ]
 
 where:
     -h  shows help
-    -p  port
     -e  environment
     -d  dns name"
 
 # Default configuration
 PORT=8080
 ENV="local"
-DOMAIN="development.vicinity.ws"
+DOMAIN="NONE"
 
 # Get configuration
 while getopts 'hd:p:e:' option; do
@@ -49,14 +48,16 @@ docker build -f Dockerfile -t nm-ui .
 # CREATE LOCAL FOLDERS
 mkdir -p ~/docker_data/logs/nginx
 # RUN
-if [ "${ENV}" == "local" ]
-then
+if [ "${ENV}" == "local" ] ; then
   docker run -d -p ${PORT}:80 \
           --name nm-ui \
           -v ~/docker_data/logs/nginx:/var/log/nginx \
           nm-ui:latest
+elif [ "${DOMAIN}" == "NONE" ] ; then
+  echo "Missing DNS name!!"
+  exit 1
 else
-  docker run -d -p ${PORT}:80 \
+  docker run -d -p 443:443 -p 80:80 \
           --name nm-ui \
           --mount type=bind,source=/etc/letsencrypt/live/${DOMAIN}/privkey.pem,target=/var/certificates/privkey.pem,readonly \
           --mount type=bind,source=/etc/letsencrypt/live/${DOMAIN}/fullchain.pem,target=/var/certificates/fullchain.pem,readonly \
