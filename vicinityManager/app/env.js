90f42bcee0df263a9847512199d56a63cd3da5c6
Jorge Almela <jorge.almela@bavenir.eu>
2019-03-12 09:07:26 +0100

    Update docker automation script

--- vicinityManager/app/env.js
+++ vicinityManager/app/env.js
@@ -1,5 +1,5 @@
 this._env = {};
 this._env.apiUrl = "https://development.vicinity.ws:3000";
-this._env.baseHref = "https://vicinity.bavenir.eu";
+this._env.baseHref = "localhost:8080";
 this._env.notificationTimeout = 30000;
 // this._env.apiUrl = "http://localhost:3000";
--- vicinityManager/run.sh
+++ vicinityManager/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-usage="$(basename "$0") [-h] [-e env -d dns_name]
+usage="$(basename "$0") [-h] [-e env -d dns_name -w workdir -p local_port -n app_name]
 -- Builds vcnt-ui docker
  -- Examples
  -- Production: ./run.sh -e prod -d www.example.com
@@ -8,16 +8,21 @@ usage="$(basename "$0") [-h] [-e env -d dns_name]
 
 where:
     -h  shows help
-    -e  environment
+    -e  environment [prod, dev, local (default)]
+    -w  workdir [~/vicinity_nm_ui (default)]
+    -p  local mode port [8080 (default)]
+    -n  app name [nm-ui (default)]
     -d  dns name"
 
 # Default configuration
 PORT=8080
 ENV="local"
-DOMAIN="NONE"
+DOMAIN=false
+WORKDIR=false
+NAME="nm-ui"
 
 # Get configuration
-while getopts 'hd:p:e:' option; do
+while getopts 'hd:d:e:w:' option; do
   case "$option" in
     h) echo "$usage"
       exit
@@ -31,36 +36,50 @@ while getopts 'hd:p:e:' option; do
     d)
       DOMAIN="$OPTARG"
       ;;
+    w)
+      WORKDIR="$OPTARG"
+      ;;
   esac
 done
 
+# Set WORKDIR
+if [ ${WORKDIR} == false ]; then
+  WORKDIR=~/vicinity_nm_ui
+fi
+
 # CLEAN OLD BUILD
-docker kill nm-ui >/dev/null 2>&1
-docker rm nm-ui >/dev/null 2>&1
-docker rmi nm-ui >/dev/null 2>&1
+docker kill ${NAME} >/dev/null 2>&1
+docker rm ${NAME} >/dev/null 2>&1
+docker rmi ${NAME} >/dev/null 2>&1
 # Copy relevant files based on ENV
 cp docker/nginx.${ENV}.conf nginx.conf
-cp docker/Dockerfile.${ENV} Dockerfile
 cp app/envs/env_${ENV}.js app/env.js
 # Docker build
 npm install
-docker build -f Dockerfile -t nm-ui .
+docker build -f ${WORKDIR}/Dockerfile -t ${NAME} ${WORKDIR}
 # CREATE LOCAL FOLDERS
 mkdir -p ~/docker_data/logs/nginx
 # RUN
 if [ "${ENV}" == "local" ] ; then
+  echo "Running in local mode, in port ${PORT}"
   docker run -d -p ${PORT}:80 \
-          --name nm-ui \
+          --name ${NAME} \
           -v ~/docker_data/logs/nginx:/var/log/nginx \
-          nm-ui:latest
-elif [ "${DOMAIN}" == "NONE" ] ; then
-  echo "Missing DNS name!!"
-  exit 1
+          ${NAME}:latest
+elif [ "${DOMAIN}" == false ] ; then
+  echo "Missing DNS name, running non-SSL mode"
+  docker run -d -p 80:80 \
+          --name ${NAME} \
+          -v ~/docker_data/logs/nginx:/var/log/nginx \
+          ${NAME}:latest
 else
+  echo "Configuration complete, running SSL mode"
   docker run -d -p 443:443 -p 80:80 \
-          --name nm-ui \
+          --name ${NAME} \
           --mount type=bind,source=/etc/letsencrypt/live/${DOMAIN}/privkey.pem,target=/var/certificates/privkey.pem,readonly \
           --mount type=bind,source=/etc/letsencrypt/live/${DOMAIN}/fullchain.pem,target=/var/certificates/fullchain.pem,readonly \
           -v ~/docker_data/logs/nginx:/var/log/nginx \
-          nm-ui:latest
+          ${NAME}:latest
 fi
+
+echo "Success! Logs stored under ~/docker_data/logs/nginx"
