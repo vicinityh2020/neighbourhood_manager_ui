bde9c1c6f020959a16253c746dd5e1d407a2f80b
Jorge Almela <jorgealmela@JorgePc.local>
2019-07-08 16:07:37 +0200

    Simplify user data fetching process; fix typo in HTML related code

--- vicinityManager/app/modules/home/controllers/home.userAccountController.js
+++ vicinityManager/app/modules/home/controllers/home.userAccountController.js
@@ -1,12 +1,11 @@
 'use strict';
 angular.module('VicinityManagerApp.controllers').
-controller('userAccountController', function($scope, $window, $cookies, commonHelpers, userAccountAPIService, AuthenticationService, Notification) {
-  $scope.name = {};
-  $scope.avatar = {};
-  $scope.occupation = {};
-  $scope.organisation = {};
-  $scope.userAccountId = {};
-  $scope.companyAccountId = {};
+controller('userAccountController', function($scope, $window, $cookies, commonHelpers, userAPIService, AuthenticationService, Notification) {
+  $scope.name = "";
+  $scope.avatar = "";
+  $scope.occupation = "";
+  $scope.userAccountId = "";
+  $scope.companyAccountId = "";
   $scope.loaded = false;
 
   // ====== Triggers window resize to avoid bug =======
@@ -24,22 +23,13 @@ controller('userAccountController', function($scope, $window, $cookies, commonHe
     console.log("End: Signout");
   };
 
-  userAccountAPIService.getUserAccountProfile($window.sessionStorage.companyAccountId)
+  userAPIService.getUser($window.sessionStorage.userAccountId)
   .then(function(response){
-      var i=0;
-      var j=0;
-      while (i === 0){
-        if (response.data.message.accountOf[j].id.email === $window.sessionStorage.username){
-          $scope.name =response.data.message.accountOf[j].id.name;
-          $scope.occupation=response.data.message.accountOf[j].id.occupation;
-          $scope.avatar =response.data.message.accountOf[j].id.avatar;
-          $scope.userAccountId = $window.sessionStorage.userAccountId;
-          i=1;
-        }
-        j++;
-      }
-      $scope.organisation = response.data.message.name;
-      $scope.companyAccountId = response.data.message._id;
+      $scope.name = response.data.message.name;
+      $scope.occupation = response.data.message.occupation;
+      $scope.avatar = response.data.message.avatar;
+      $scope.userAccountId = $window.sessionStorage.userAccountId;
+      $scope.companyAccountId = $window.sessionStorage.companyAccountId;
       $scope.loaded = true;
   })
   .catch(function(err){
